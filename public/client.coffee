c0bf25ce44bc69a451ad8ea3d68c06e8adf05781
Hristo Hristov <ribeto@gmail.com>
2011-06-25 16:00:00 -0400

    Adding faye and some initial client code

--- public/client.coffee
+++ public/client.coffee
@@ -1,1 +1,37 @@
-console.log "Hello, World!"
+client = new Faye.Client "http://localhost:3000/faye"
+
+sessionId = null
+apiKey = null
+token = null
+session = null
+
+addHandler = (session,type,callback) ->
+	session.addEventListener type, callback
+	
+subscribeToStreams = (streams) ->
+	for stream in streams
+		session.subscribe stream if stream.connection.connectionId != session.connection.connectionId 
+		
+
+setupSession = (session) ->
+	addHandler session, "sessionConnected", (event) -> 
+		console.log "sessionConnected"
+		subsribeToStreams event.streams
+		session.publish()
+		
+	addHandler session, "streamCreated", (event) -> 
+		console.log "streamCreated"
+		subsribeToStreams event.streams
+	
+
+connectOpenTok = () ->
+	session = TB.initSession(sessionId)
+	session.connect(apiKey,token)
+	
+	setupSession session
+
+client.subscribe "/yourface", (message) ->
+	console.log "faye message -> #{message}"
+	sessionId = message.sessionId
+	apiKey = message.apiKey
+	token = message.token
--- public/client.js
+++ public/client.js
@@ -1,3 +1,42 @@
 (function() {
-  console.log("Hello, World!");
+  var addHandler, apiKey, client, connectOpenTok, session, sessionId, setupSession, subscribeToStreams, token;
+  client = new Faye.Client("http://localhost:3000/faye");
+  sessionId = null;
+  apiKey = null;
+  token = null;
+  session = null;
+  addHandler = function(session, type, callback) {
+    return session.addEventListener(type, callback);
+  };
+  subscribeToStreams = function(streams) {
+    var stream, _i, _len, _results;
+    _results = [];
+    for (_i = 0, _len = streams.length; _i < _len; _i++) {
+      stream = streams[_i];
+      _results.push(stream.connection.connectionId !== session.connection.connectionId ? session.subscribe(stream) : void 0);
+    }
+    return _results;
+  };
+  setupSession = function(session) {
+    addHandler(session, "sessionConnected", function(event) {
+      console.log("sessionConnected");
+      subsribeToStreams(event.streams);
+      return session.publish();
+    });
+    return addHandler(session, "streamCreated", function(event) {
+      console.log("streamCreated");
+      return subsribeToStreams(event.streams);
+    });
+  };
+  connectOpenTok = function() {
+    session = TB.initSession(sessionId);
+    session.connect(apiKey, token);
+    return setupSession(session);
+  };
+  client.subscribe("/yourface", function(message) {
+    console.log("faye message -> " + message);
+    sessionId = message.sessionId;
+    apiKey = message.apiKey;
+    return token = message.token;
+  });
 }).call(this);
